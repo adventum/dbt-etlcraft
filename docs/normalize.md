d8d9774e5f2f09d742f73f05985ce1e7636f9a76
Nikolay Suschenko <n.sushchenko@adventum.ru>
2023-07-19 10:13:33 +0300

    Добавил аргумент source_table в normalize

--- docs/normalize.md
+++ docs/normalize.md
@@ -5,14 +5,15 @@ The `normalize` macro is designed to normalize tables that are downloaded by Air
 1. `included_fields` (default: empty list)
 2. `excluded_fields` (default: empty list)
 3. `defaults_dict` (default: result of `etlcraft_defaults()` macro)
+4. `source_table` (default: none)
 
 ## Macro behavior
 
 The behavior of the `normalize` macro depends on the name of the model. The model name must follow the pattern `normalize_{sourcetypename}_{templatename}_{streamname}` with an optional suffix `_auto`. The macro extracts `sourcetypename`, `templatename`, and `streamname`, and creates a union of the tables `_airbyte_{sourcetypename}_%_{templatename}_{streamname}`.
 
-The macro utilizes `dbt_utils.union(dbt_utils.get_relations_by_pattern(schema_pattern, table_pattern)))` to get all the relevant tables. `schema_pattern` is the name of the current schema and `table_pattern` is as above.
+If `source_table` argument is not provided, the macro utilizes `dbt_utils.union(dbt_utils.get_relations_by_pattern(schema_pattern, table_pattern)))` to get all the relevant tables if. `schema_pattern` is the name of the current schema and `table_pattern` is as above.
 
-These tables contain columns `_airbyte_ab_id`, `_airbyte_data` and `_airbyte_emitted_at`. `_airbyte_data` is a JSON field that contains the data to be normalized. The macro looks at the first line of the first column and detects the list of keys in the JSON field.
+These tables contain columns `_airbyte_ab_id`, `_airbyte_data` and `_airbyte_emited_at`. `_airbyte_data` is a JSON field that contains the data to be normalized. The macro looks at the first line of the first column and detects the list of keys in the JSON field.
 
 Then the macro iterates over that list and for each key, adds a line to a column list of the SELECT query to be returned. The generated line uses the macro `json_extract_string(_airbyte_emitted_at, {key_name})` to get the corresponding key from the JSON field. It also contains an AS expression, that names the column as the `{key_name}`, but after transformation with the macro `normalize_name(key_name)` that removes spaces, transliterates Cyrillic symbols, etc.
 
--- macros/normalize.sql
+++ macros/normalize.sql
@@ -1,26 +1,7 @@
-/* normalize -- a dbt macros that normalizes tables downloaded by Airbyte.
-
-The behaviour of the macros depends of the name of them model. The model name must follow the pattern
-normalize_{sourcetypename}_{templatename}_{streamname} with optional suffix _auto.
-Macros extracts sourcetypename, templatename and streamname, and makes union of the tables
-_airbyte_{sourcetypename}_%_{templatename}_{streamname}. 
-
-Macros utilizes dtb_utils.union(dbt_utils.get_relations_by_pattern(schema_pattern, table_pattern)))
-to get all the relevant tables. schema_pattern is the name of the current schema and table_pattern is as above.
-
-These tables contains columns _airbyte_ab_id, _airbyte_data and _airbyte_emited_at. _airbyte_data
-is a JSON field that contains the data to be normalized. Macros looks at the first line of the first 
-column and detects the list of keys in the JSON field.
-
-Then macros iterates over that list and for each adds a line to a column list of the SELECT query to be returned. that extracts the corresponding value from JSON as a column and names. It names
-The generated line uses macros json_extract_string(_airbyte_emited_ad, {key_name}) that get the correspoding 
-key from the JSON field. Also it contains AS expression, that name the column as the {key_name}, but
-after transofmation macros normalize_name(key_name) that removes spaces, transliterates Cyrillic symbols, etc.
-*/
-{%- macro normalize() -%}
+{%- macro normalize(source_table=none, included_fields=[], excluded_fields=[], defaults_dict=etlcraft_defaults()) -%}
 {%- if execute -%}
     {%- set model_name_parts = this.name.split('_') -%}
-    {%- if model_name_parts|length < 5 or model_name_parts[0] != 'normalize' or (model_name_parts[-1] != 'auto' and model_name_parts[-1] != 'manual') -%}
+    {%- if model_name_parts|length < 5 or model_name_parts[0] != 'normalize' -%}
         {{ exceptions.raise_compiler_error('Model name "' ~ this.name ~ '" does not follow the expected pattern: "normalize_{sourcetype}_{templatename}_{streamname}(__auto)?", where suffix is "auto" is optional') }}
     {%- endif -%}
     {%- set source_type = model_name_parts[1] -%}
@@ -29,22 +10,26 @@ after transofmation macros normalize_name(key_name) that removes spaces, transli
     {%- set stream_name = '_'.join(stream_name_parts) -%}
     {%- set schema_pattern = this.schema -%}
     {%- set table_pattern = '_airbyte_raw_' ~ source_type ~ '_%_' ~ template_name ~ '_' ~ stream_name -%}
-    {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=target.schema, 
-                                                          table_pattern=table_pattern) -%}
-    {%- if relations|length < 1 -%}
-        {{ exceptions.raise_compiler_error('No relations were found matching the pattern "' ~ table_pattern ~ '". Please ensure that your source data follows the expected structure.') }}
+
+    {%- if source_table is none -%}
+        {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=target.schema, 
+                                                              table_pattern=table_pattern) -%}
+        {%- if relations|length < 1 -%}
+            {{ exceptions.raise_compiler_error('No relations were found matching the pattern "' ~ table_pattern ~ '". Please ensure that your source data follows the expected structure.') }}
+        {%- endif -%}
+        {%- set source_table = dbt_utils.union_relations(relations) -%}    
     {%- endif -%}
-    {%- set unioned_tables = dbt_utils.union_relations(relations) -%}    
-    {%- set json_keys = fromjson(run_query('SELECT ' ~ json_list_keys('_airbyte_data') ~ ' FROM ' ~ unioned_tables ~ ' LIMIT 1').columns[0].values()[0])  -%}    
+
+    {%- set json_keys = fromjson(run_query('SELECT ' ~ json_list_keys('_airbyte_data') ~ ' FROM ' ~ source_table ~ ' LIMIT 1').columns[0].values()[0])  -%}    
     {%- set column_list = [] -%}
     {%- for key in json_keys -%}
         {%- do column_list.append(json_extract_string('_airbyte_data', key) ~ " AS " ~ normalize_name(key)) -%}
     {%- endfor -%}
     SELECT
-        _dbt_source_relation,
-        _airbyte_ab_id, 
-        _airbyte_emitted_at,
+        _dbt_source_relation AS _table_name,        
+        _airbyte_emited_at AS _emited_at,
+        NOW() as _normalized_at,
         {{ column_list|join(', \n') }}
-    FROM {{ unioned_tables }}
+    FROM {{ source_table }}
 {%- endif -%}
 {%- endmacro -%}
