907bf02c672b8ae1e95ec04608720d1fed12afbf
Nikolay Suschenko <n.sushchenko@adventum.ru>
2023-07-27 14:44:57 +0300

    Добавил тест для find_incremental_datetime_field

--- macros/find_incremental_datetime_field.sql
+++ macros/find_incremental_datetime_field.sql
@@ -1,4 +1,4 @@
-{% macro find_incremental_datetime_field(relation, defaults_dict=etlcraft_defaults()) %}
+{% macro find_incremental_datetime_field(relation, defaults_dict=etlcraft_defaults(), override_column_list=None) %}
     {%- set model_name_parts = relation.split('_') -%}
     {%- if model_name_parts|length < (6 if model_name_parts[-1] == 'auto' else 4) or model_name_parts[0] != 'normalize' -%}
         {{ exceptions.raise_compiler_error('Relation name "' ~ relation ~ '" does not follow the expected pattern: "{stagename}_{sourcetype}_{templatename}_{streamname}(__auto)?", where suffix "__auto" is optional') }}
@@ -7,31 +7,31 @@
     {%- set stream_name_parts = model_name_parts[3:-1] if model_name_parts[-1] == 'auto' else model_name_parts[3:] -%}
     {%- set stream_name = '_'.join(stream_name_parts) -%}
     
-    {%- set datetime_field = get_from_default_dict(defaults_dict, ['sourcetypes', source_type, 'streams', stream_name, 'datetime_field']) -%}
+    {%- set datetime_field = get_from_default_dict(defaults_dict, ['sourcetypes', source_type, 'streams', stream_name, 'incremental_datetime_field']) -%}
     {%- if datetime_field == {} -%}
         {%- set datetime_field = get_from_default_dict(defaults_dict, ['sourcetypes', source_type, 'incremental_datetime_field']) -%}
     {%- endif -%}
     
     {%- if datetime_field == {} -%}
         {%- set datetime_columns = [] -%}
-        {%- set columns = adapter.get_columns_in_relation(relation) -%}
-        {%- for column in columns -%}            
+        {%- set columns = adapter.get_columns_in_relation(relation) if not override_column_list else override_column_list -%}
+        {%- for column in columns -%}           
             {%- if  column.name not in ['_emited_at', '_normalized_at'] and (
-              column.column.lower().endswith('date') 
-              or column.column.lower().endswith('datetime')
-              or column.column.lower().endswith('timestamp') 
-              or column.data_type.lower().endswith('date') 
-              or column.data_type.lower().endswith('datetime')
-              or column.data_type.lower().endswith('timestamp')) -%}
-                {%- do datetime_columns.append(column.column) -%}
+              column.name.lower().endswith('date') 
+              or column.name.lower().endswith('datetime')
+              or column.name.lower().endswith('timestamp') 
+              or column.dtype.lower().endswith('date') 
+              or column.dtype.lower().endswith('datetime')
+              or column.dtype.lower().endswith('timestamp')) -%}
+                {%- do datetime_columns.append(column) -%}
             {%- endif -%}
         {%- endfor -%}
-        {%- if datetime_columns|length == 0 -%}
+        {%- if datetime_columns|length == 0 and not do_not_throw -%}
             {{ exceptions.raise_compiler_error('No columns with names or types ending in "date" or "datetime" were found in the relation "' ~ relation ~ '". Please ensure that your source data contains such a column for incremental processing.') }}
-        {%- elif datetime_columns|length > 1 -%}
+        {%- elif datetime_columns|length > 1 and not do_not_throw -%}
             {{ exceptions.raise_compiler_error('Multiple columns with names or types ending in "date" or "datetime" were found in the relation "' ~ relation ~ '". Please ensure that only one such column is present for incremental processing, or specify the correct one in the defaults dictionary.') }}
         {%- else -%}
-            {%- set datetime_field = datetime_columns[0] -%}
+            {%- set datetime_field = datetime_columns[0].name -%}
         {%- endif -%}
     {%- endif -%}
     
---
+++ tests/test_find_incremental_datetime_field.sql
@@ -0,0 +1,58 @@
+{% set table_name = "normalize_testsourcetypename_testtemplatename_teststreamname" %}
+{% set column_list1 = [api.Column.create('id', 'UInt32'), api.Column.create('name', 'String'),
+    api.Column.create('created_at', 'DateTime'), 
+    api.Column.create('_emited_at', 'DateTime'), api.Column.create('_normalized_at', 'DateTime')] %}
+{% set column_list2 = [api.Column.create('id', 'UInt32'), api.Column.create('name', 'String'), 
+    api.Column.create('created_at', 'DateTime'), api.Column.create('updated_at', 'DateTime'),
+    api.Column.create('_emited_at', 'DateTime'), api.Column.create('_normalized_at', 'DateTime')] %}
+{% set column_list3 = [api.Column.create('id', 'UInt32'), api.Column.create('name', 'String'), 
+    api.Column.create('MyDatetime', 'String'),
+    api.Column.create('_emited_at', 'DateTime'), api.Column.create('_normalized_at', 'DateTime')] %}
+
+SELECT
+'Error: Incorrect datetime field identified without dict by type'
+WHERE '{{ find_incremental_datetime_field(table_name~"1", override_column_list=column_list1) }}' != 'created_at'
+
+UNION ALL SELECT
+'Error: Incorrect datetime field identified without dict by name'
+WHERE '{{ find_incremental_datetime_field(table_name~"2", override_column_list=column_list3) }}' != 'MyDatetime'
+
+
+{% set my_dict %}
+sourcetypes:
+  testsourcetypename:
+    incremental_datetime_field: updated_at
+{% endset %}
+
+UNION ALL SELECT
+'Error: Incorrect datetime field identified with dict for source type'
+WHERE '{{ find_incremental_datetime_field(table_name~"3", defaults_dict=fromyaml(my_dict), 
+            override_column_list=column_list2) }}' != 'updated_at'
+
+{% set my_dict2 %}
+sourcetypes:
+  testsourcetypename:
+    incremental_datetime_field: updated_at
+    streams:
+      teststreamname4:
+        incremental_datetime_field: created_at
+{% endset %}
+
+UNION ALL SELECT
+'Error: Incorrect datetime field identified with dict for stream'
+WHERE '{{ find_incremental_datetime_field(table_name~"4", defaults_dict=fromyaml(my_dict2),
+            override_column_list=column_list2) }}' != 'created_at'
+
+{% set my_dict3 %}
+sourcetypes:
+  testsourcetypename:
+    incremental_datetime_field: updated_at
+    streams:
+      teststreamname5:
+        incremental_datetime_field: False
+{% endset %}
+
+UNION ALL SELECT
+'Error: Incorrect datetime field identified when there is no incremental field'
+WHERE '{{ find_incremental_datetime_field(table_name~"5", defaults_dict=fromyaml(my_dict3), 
+            override_column_list=column_list2) == False }}' != 'True'
