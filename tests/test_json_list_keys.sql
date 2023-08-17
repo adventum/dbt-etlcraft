f8cd135deb8541d3ece577e3487f0e0472b973a6
Nikolay Suschenko <n.sushchenko@adventum.ru>
2023-08-17 10:03:50 +0300

    Исправлены варнинги

--- tests/test_json_list_keys.sql
+++ tests/test_json_list_keys.sql
@@ -1,6 +1,4 @@
  SELECT 'The key set doesn''t match'
-    FROM {{ ref('_airbyte_raw_alytics_11222_testtemplate_project_goals') }} 
+    FROM (SELECT '{"access": 0, "custom_interaction": 0, "goal_id": 0}' AS _airbyte_data) 
     WHERE {{ json_list_keys("_airbyte_data") }} != {{ array(["'access'", "'custom_interaction'", 
-         "'goal_id'", "'in_service_id'", "'in_service_id_for_stat'", "'interaction_type'", 
-         "'interaction_type_id'", "'is_active'", "'is_economic'", "'resource_uri'", 
-         "'service_title'", "'terms'", "'title'", "'__clientName'", "'__productName'"]) }}
+         "'goal_id'"]) }}
