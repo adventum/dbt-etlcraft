9a0cff4112f6159dace49d5ef1889399d2850509
Nikolay Suschenko <n.sushchenko@adventum.ru>
2023-08-18 18:53:48 +0300

    Fix normalize

--- macros/normalize.sql
+++ macros/normalize.sql
@@ -52,7 +52,7 @@
     SELECT
         {{ column_list | join(', \n') }},
         toLowCardinality(_dbt_source_relation) AS __table_name,        
-        toDateTime(substring(_airbyte_emitted_at, 1, 19)) AS __emitted_at,
+        toDateTime64(_airbyte_emitted_at, 1, 19) AS __emitted_at,
         NOW() as __normalized_at
     FROM {{ source_table }}
 {%- endif -%}
