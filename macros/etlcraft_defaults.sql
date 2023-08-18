ed5aad91e2195a841246ed4da1859da12c38883a
Nikolay Suschenko <n.sushchenko@adventum.ru>
2023-08-18 18:35:23 +0300

    Добавил appsflyer в etlcraft_defaults

--- macros/etlcraft_defaults.sql
+++ macros/etlcraft_defaults.sql
@@ -4,6 +4,10 @@ sourcetypes:
   example:
     included_fields:
     - test_field
+  appsflyer:
+    streams:
+      in_app_events:
+        incremental_datetime_field: event_time
 {% endset %}
   {{ return(fromyaml(etlcraft_defaults_dict)) }}
 {% endmacro %}
