4b5b8c12a60c5824af9b6664e38475a760e2840e
Pavel Lizunov <p.lizunov@adventum.ru>
2023-11-17 13:01:24 +0300

    Перенос join макросов в draft

--- macros/join_pipeline_name.sql
+++
@@ -1,9 +0,0 @@
-{%- macro join_pipeline_name(macro_name, source_types, streams, templates) -%}
-    
-    {% if macro_name == 'mobile_event' %}
-        {{etlcraft.join_mobile_event(source_types, streams, templates)}}
-    {% elif macro_name == 'mobile_install' %}
-        {{etlcraft.join_mobile_install(source_types, streams, templates)}}
-    {% endif %}
-    
-{%- endmacro -%}
