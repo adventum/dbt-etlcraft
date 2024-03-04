a6c07532add5b70c21e954741bb63203e7578875
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-03-04 14:41:54 +0300

    изменения в combine_events_default - соединение через макрос union_relations

--- macros/combine_events_default.sql
+++ macros/combine_events_default.sql
@@ -7,64 +7,15 @@
     params
     ) -%}
 
-SELECT  
-__date,
-event_datetime,
-accountName,
-__table_name,
-appmetricaDeviceId,
-mobileAdsId,
-crmUserId,
-transactionId,
-promoCode,
-osName,
-cityName,
-addToCartSessions,
-cartViewSessions,
-checkoutSessions,
-webSalesSessions,
-sales,
-amountSales,
-registrationCardSessions,
-registrationButtonClick,
-linkingCardToPhoneNumberSessions,
-registrationCashbackSessions,
-instantDiscountActivationSessions,
-couponActivationSessions,
-participationInLotterySessions,
-screenView,
-__emitted_at
-FROM {{ ref('join_appmetrica_events_default_events') }}
-UNION ALL
-
-SELECT  
-__date,
-event_datetime,
-accountName,
-__table_name,
-appmetricaDeviceId,
-mobileAdsId,
-crmUserId,
-transactionId,
-promoCode,
-osName,
-cityName,
-addToCartSessions,
-cartViewSessions,
-checkoutSessions,
-webSalesSessions,
-sales,
-amountSales,
-registrationCardSessions,
-registrationButtonClick,
-linkingCardToPhoneNumberSessions,
-registrationCashbackSessions,
-instantDiscountActivationSessions,
-couponActivationSessions,
-participationInLotterySessions,
-screenView,
-__emitted_at
-FROM {{ ref('join_appmetrica_events_default_screen_view') }}
+SELECT *
+FROM 
+{{ dbt_utils.union_relations(
+      relations=[
+        ref('join_appmetrica_events_default_events'),
+        ref('join_appmetrica_events_default_screen_view'),
+      ],
+      exclude=['_DBT_SOURCE_RELATION']
+  ) }}
 
 
 {% endmacro %}
