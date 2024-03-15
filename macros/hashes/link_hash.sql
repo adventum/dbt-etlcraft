b78ac7f0093872f2f0b67ac21b182d5762bb0528
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-03-15 13:03:44 +0300

    комментарии в link_hash

--- macros/hashes/link_hash.sql
+++ macros/hashes/link_hash.sql
@@ -1,12 +1,17 @@
+{#- макрос берёт на вход имя линка и данные из файла metadata -#} 
 {%- macro link_hash(link_name, metadata_dict) -%}
+    {#- задаём имена сущности, линка - из словаря metadata -#}
     {%- set entities = metadata_dict['entities'] -%}
     {%- set links = metadata_dict['links'] -%}
+    {#- имя хэша задаём из входного аргумента, оборачиваем в кавычки -#}
     {%- set name_hash = "'" ~ link_name ~ "'" -%}
 
+    {#- для заданного линка берём его сущности  и ключи-#}
     {%- set link_entities = links[link_name]['entities'] -%} {# связанные линком хабы #}
     {%- set link_keys = links[link_name].get('keys') -%} {# доп. ключи для линка #}
     {%- set all_keys = [] -%}
 
+    {#- для каждой сущности заданного линка берем его ключи -#}
     {%- for entity in link_entities -%}
         {%- set _ = all_keys.extend(entities[entity]['keys']) -%} {# собираем список ключей хабов #}
     {%- endfor -%}
@@ -18,16 +23,21 @@
     {%- set all_cols = [name_hash] -%} {# сюда складываем ключи с трансформациями #}
     {%- set skip_null_cols = [] -%} {# skipInNullCheck #}
 
+    {#- тут происходит техническая предобработка данных для всех ключей -#}
     {%- for key in all_keys -%}
-        {%- set sql_string = 'toString({})'.format(key['name']) -%}
+        {#- приводим к формату String -#}
+        {%- set sql_string = 'toString({})'.format(key['name']) -%} 
         
+        {#- если нет ограничения, убираем пробелы по краям -#}
         {%- if not key.get('notrim') -%}
             {%- set sql_string = 'trim({})'.format(sql_string) -%}
         {%- endif -%}
+        {#- если нет ограничения, приводим всё к верхнему регистру -#}
         {%- if not key.get('case-sensitive') -%}
             {%- set sql_string = 'upper({})'.format(sql_string) -%}
         {%- endif -%}
 
+        {#- обработка в случае null -#}
         {%- set sql_string = 'ifnull(nullif({}, \'\'), \'\')'.format(sql_string) -%}
 
         {%- if not key.get('skipInNullCheck') -%}
@@ -41,7 +51,7 @@
     {%- set all = ' || \';\' || '.join(all_cols) -%}
     {%- set skip_null = ' || '.join(skip_null_cols) -%}
 
-
+    {#-  здесь то, что получается в результате -#}
     assumeNotNull(coalesce(if({{ skip_null }} = '', null, hex(MD5({{ all }}))))) as {{ link_name }}Hash
     
 {%- endmacro -%}
