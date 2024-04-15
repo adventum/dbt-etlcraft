cf4e0715e58a2b34f057a98fd5a09aa22a6812ec
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-04-15 18:53:39 +0300

    изменения в макросе link: инкрементальность, более лаконичный код

--- macros/link/link.sql
+++ macros/link/link.sql
@@ -1,4 +1,4 @@
-{%- macro link() %}
+{%- macro link(disable_incremental=False) %}
 
 {% set metadata = fromyaml(etlcraft.metadata()) %}
 
@@ -11,14 +11,44 @@
 {#- задаём пустой список: сюда будем добавлять колонки, по которым будем делать GROUP BY -#}
 {% set group_by_fields = [] %}
 
+{#- задаём по возможности инкрементальность -#}
+
+{%- set columns_names_with_date_type = [] -%}
+{% for c in adapter.get_columns_in_relation(load_relation(ref(source_model_name))) -%}
+{%- if 'Date' in c.data_type or 'DateTime' in c.data_type -%}
+{%- do columns_names_with_date_type.append(c.name)  -%}
+{%- endif -%}
+{%- endfor -%} 
+{%- if '__date' in columns_names_with_date_type -%}
+
+{{ config(
+    materialized='incremental',
+    order_by=('__date', '__table_name'),
+    incremental_strategy='delete+insert',
+    unique_key=['__date', '__table_name'],
+    on_schema_change='fail'
+) }}
+
+{#- если не установлено - будем делать view -#}
+{%- else -%}
+
+{{ config(
+    materialized='view'
+) }}
+
+{%- endif -%}
+
+{#- задаём наименования числовых типов данных -#}
+{%- set numeric_types = ['Int', 'Float', 'Num'] -%}
+
 {#- для каждой колонки таблицы, на которую будем ссылаться -#}
 SELECT {% for c in adapter.get_columns_in_relation(load_relation(ref(source_model_name))) -%}
 {#- если тип данных колонки не числовой - добавляем её в список для будущей группировки -#}
-{% if 'String' in c.data_type or 'LowCardinality' in c.data_type or 'Date' in c.data_type or 'DateTime' in c.data_type %}{{ c.name }}
+{% if c.data_type not in numeric_types %}{{ c.name }}
 {%- do group_by_fields.append(c.name)  -%}
 {#- а если тип данных колонки числовой - суммируем данные по ней -#}
-{%- elif 'Int' in c.data_type or 'Float' in c.data_type or 'Num' in c.data_type %}SUM({{ c.name }}) AS {{ c.name }}
-{#- а для остальных (т.е. колонок не числового типа данных) - делаем MAX - просто чтобы избежать дублей -#}
+{%- elif c.data_type in numeric_types %}SUM({{ c.name }}) AS {{ c.name }}
+{#- а для остальных (т.е. колонок не числового типа данных) - делаем MAX - чтобы избежать дублей -#}
 {%- else %} MAX({{ c.name }}) AS {{ c.name }}
 {#- после каждой строки кроме последней расставляем запятые, чтобы сгенерировался читаемый запрос -#}
 {%- endif %}{% if not loop.last %},{% endif %}{% endfor %} 
