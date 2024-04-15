24abe3aa195c0cdc8bc0dd7c765472390e53a693
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-04-15 17:15:47 +0300

    разделение combine на отдельные модели для registry

--- macros/combine/combine.sql
+++ macros/combine/combine.sql
@@ -10,7 +10,8 @@
 
 {#- задаём части имени - pipeline это например datestat -#}
 {%- set model_name_parts = (override_target_model_name or this.name).split('_') -%}
-{%- set pipeline_name = model_name_parts[1] -%}
+{#- в combine могут быть модели из 2 или трех частей, но последняя часть всегда обозначает пайплайн -#}
+{%- set pipeline_name = model_name_parts[-1] -%}
 
 {#- если имя модели не соответсвует шаблону - выдаём ошибку -#}
 {%- if model_name_parts|length < 2 or model_name_parts[0] != 'combine' -%}
@@ -18,7 +19,14 @@
 {%- endif -%}
 
 {#- задаём паттерн, чтобы найти все join-таблицы нужного пайплайна -#}
-{%- set table_pattern = 'join' ~ '_[^_]+_' ~ pipeline_name -%}  
+{#- для обычных направлений, состоящих из двух частей имени -#}
+{%- if model_name_parts|length < 3 -%}
+{%- set table_pattern = 'join' ~ '_[^_]+_' ~ pipeline_name -%} 
+{#- для направления registry, где в combine данные должны идти отдельно по типам источника -#}
+{%- else -%} 
+{%- set sourcetype_name = model_name_parts[1] -%} 
+{%- set table_pattern = 'join_' ~ sourcetype_name ~ '_' ~ pipeline_name -%} 
+{%- endif -%}
 
 {#- находим все таблицы, которые соответствут паттерну -#}
 {%- set relations = etlcraft.get_relations_by_re(schema_pattern=target.schema, 
@@ -31,11 +39,8 @@
 
 {#- собираем одинаковые таблицы, которые будут проходить по этому макросу  - здесь union all найденных таблиц -#}
 
-{#- было вот так, через макрос dbt_utils.union_relations - но из-за null были ошибки "Cannot convert NULL to a non-nullable type"
-{%- set source_table = '(' ~ dbt_utils.union_relations(relations, source_column_name=none) ~ ')' -%} -#}
-
-{#- стало вот так, через кастомный макрос, чтобы null заменить на '' или 0  -#}
-{%- set source_table = '(' ~ etlcraft.custom_union_relations(relations, source_column_name=none) ~ ')' -%}
+{#- делаем это через кастомный макрос, чтобы null заменить на '' или 0  -#}  {#- relations, source_column_name=None  - без этого ошибка -#}
+{%- set source_table = '(' ~ etlcraft.custom_union_relations(relations, source_column_name=None) ~ ')' -%}
 
 {#- если не писать варианты, то делать так - сейчас мы используем этот вариант -#}
 
