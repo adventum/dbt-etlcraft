668399737f5911b58a41d2231cdfb7cdf8bd7bb6
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-04-17 13:29:26 +0300

    проверка работы проекта + изменение в metadata

--- integration_tests/target/compiled/etlcraft_integration_tests/models/4_business/full_events.sql
+++ integration_tests/target/compiled/etlcraft_integration_tests/models/4_business/full_events.sql
@@ -1,51 +1,31 @@
 -- depends_on: test.graph_qid
 -- depends_on: test.link_appmetrica_registry
-SELECT * FROM 
 
-        (
-            select
 
-                --toLowCardinality('graph_qid')  as _dbt_source_relation,
-                
-                            toString("__link") as __link ,
-                            toDateTime("__datetime") as __datetime ,
-                            toString("__id") as __id ,
-                            toUInt64("qid") as qid ,
-                            toString('') as appmetricaDeviceId ,
-                            toString('') as crmUserId ,
-                            toString('') as cityName ,
-                            toDateTime(0) as __emitted_at ,
-                            toString('') as AppProfileMatchingHash ,
-                            toString('') as AppMetricaDeviceHash ,
-                            toString('') as CrmUserHash 
-
-            from test.graph_qid
-        )
-
-        union all
-        
+SELECT * 
+FROM (
+    SELECT * FROM test.link_events
+    LEFT JOIN test.graph_qid USING (__id, __link, __datetime)
+) t1
+LEFT JOIN 
 
         (
             select
 
                 --toLowCardinality('link_appmetrica_registry')  as _dbt_source_relation,
                 
-                            toString("__link") as __link ,
-                            toDateTime("__datetime") as __datetime ,
-                            toString("__id") as __id ,
-                            toUInt64(0) as qid ,
                             toString("appmetricaDeviceId") as appmetricaDeviceId ,
                             toString("crmUserId") as crmUserId ,
                             toString("cityName") as cityName ,
                             toDateTime("__emitted_at") as __emitted_at ,
+                            toString("__link") as __link ,
                             toString("AppProfileMatchingHash") as AppProfileMatchingHash ,
                             toString("AppMetricaDeviceHash") as AppMetricaDeviceHash ,
-                            toString("CrmUserHash") as CrmUserHash 
+                            toString("CrmUserHash") as CrmUserHash ,
+                            toString("__id") as __id ,
+                            toDateTime("__datetime") as __datetime 
 
             from test.link_appmetrica_registry
         )
 
-        
-
-
-
+         t2 USING (__id, __link, __datetime)
--- integration_tests/target/run/etlcraft_integration_tests/models/2_staging/2_combine/combine_appmetrica_registry.sql
+++ integration_tests/target/run/etlcraft_integration_tests/models/2_staging/2_combine/combine_appmetrica_registry.sql
@@ -21,22 +21,5 @@ FROM (
             from test.join_appmetrica_registry
         )
 
-        union all
-        
-
-        (
-            select
-
-                --toLowCardinality('join_appmetrica_registry_app_profile_matching')  as None,
-                
-                            toString("appmetricaDeviceId") as appmetricaDeviceId ,
-                            toString("crmUserId") as crmUserId ,
-                            toString("cityName") as cityName ,
-                            toDateTime("__emitted_at") as __emitted_at ,
-                            toString("__link") as __link 
-
-            from test.join_appmetrica_registry_app_profile_matching
-        )
-
         )
   )
