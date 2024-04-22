4fbf62fe27874488a726cc553300005c6ea76345
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-04-22 09:53:13 +0300

    materialized +periodstat для условий в combine и hash

--- macros/combine/combine.sql
+++ macros/combine/combine.sql
@@ -13,17 +13,17 @@
 
 {#- если имя модели не соответсвует шаблону - выдаём ошибку -#}
 {%- if model_name_parts|length < 2 or model_name_parts[0] != 'combine' -%}
-{{ exceptions.raise_compiler_error('Model name "' ~ this.name ~ '" does not follow the expected pattern: "combine_{pipeline_name}"') }}
+  {{ exceptions.raise_compiler_error('Model name "' ~ this.name ~ '" does not follow the expected pattern: "combine_{pipeline_name}"') }}
 {%- endif -%}
 
 {#- задаём паттерн, чтобы найти все join-таблицы нужного пайплайна -#}
 {#- для обычных направлений, состоящих из двух частей имени -#}
 {%- if model_name_parts|length < 3 -%}
-{%- set table_pattern = 'join' ~ '_[^_]+_' ~ pipeline_name -%} 
+  {%- set table_pattern = 'join' ~ '_[^_]+_' ~ pipeline_name -%} 
 {#- для направления registry, где в combine данные должны идти отдельно по типам источника -#}
 {%- else -%} 
-{%- set sourcetype_name = model_name_parts[1] -%} 
-{%- set table_pattern = 'join_' ~ sourcetype_name ~ '_' ~ pipeline_name -%} 
+  {%- set sourcetype_name = model_name_parts[1] -%} 
+  {%- set table_pattern = 'join_' ~ sourcetype_name ~ '_' ~ pipeline_name -%} 
 {%- endif -%}
 
 {#- находим все таблицы, которые соответствут паттерну -#}
@@ -32,7 +32,7 @@
 
 {#- если что-то не так - выдаём ошибку                                                                  
 {%- if not relations -%}
-{{ exceptions.raise_compiler_error('No relations were found matching the pattern "' ~ table_pattern ~ '". Please ensure that your source data follows the expected structure.') }}
+  {{ exceptions.raise_compiler_error('No relations were found matching the pattern "' ~ table_pattern ~ '". Please ensure that your source data follows the expected structure.') }}
 {%- endif -%} -#} 
 
 {#- собираем одинаковые таблицы, которые будут проходить по этому макросу  - здесь union all найденных таблиц -#}
@@ -41,29 +41,27 @@
 {%- set source_table = '(' ~ etlcraft.custom_union_relations(relations, source_column_name=None) ~ ')' -%}
 
 {#- задаём по возможности инкрементальность -#}
-{%- if pipeline_name in ('datestat', 'events') -%}
+{%- if pipeline_name in ('datestat', 'events', 'periodstat') -%}
 
-{{ config(
-    materialized='incremental',
-    order_by=('__date', '__table_name'),
-    incremental_strategy='delete+insert',
-    unique_key=['__date', '__table_name'],
-    on_schema_change='fail'
-) }}
+  {{ config(
+      materialized='incremental',
+      order_by=('__date', '__table_name'),
+      incremental_strategy='delete+insert',
+      unique_key=['__date', '__table_name'],
+      on_schema_change='fail'
+  ) }}
 
-{%-else -%}
+{%- else -%}
 
-{{ config(
-    materialized='table',
-    order_by=('__table_name'),
-    on_schema_change='fail'
-) }}
+  {{ config(
+      materialized='table',
+      order_by=('__table_name'),
+      on_schema_change='fail'
+  ) }}
 
-{%-endif -%}
+{%- endif -%} 
 
 SELECT * REPLACE(toLowCardinality(__table_name) AS __table_name)
 FROM {{ source_table }} 
 
 {% endmacro %}
-
-
