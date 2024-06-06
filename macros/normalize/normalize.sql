27ce618e26fd38c24e8e5d314d789d18ce460e03
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-06-06 11:53:19 +0300

    toString в normalize для __emitted_at

--- macros/normalize/normalize.sql
+++ macros/normalize/normalize.sql
@@ -87,7 +87,7 @@
 SELECT
         {{ column_list | join(', \n        ') }},
         toLowCardinality(_dbt_source_relation) AS __table_name,  
-        toDateTime32(substring(_airbyte_extracted_at, 1, 19)) AS __emitted_at, 
+        toDateTime32(substring(toString(_airbyte_extracted_at), 1, 19)) AS __emitted_at, 
         NOW() AS __normalized_at
 FROM {{ source_table }}
 
