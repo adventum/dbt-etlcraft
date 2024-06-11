d350ab8f1952756466a84b06fa0842b80fec1748
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-06-11 10:39:33 +0300

    схема для normalize

--- macros/other/make_date.sql
+++
@@ -1,5 +0,0 @@
-{% macro make_date_zero(date) -%}
-toDate ( 
-            replaceRegexpOne (replaceRegexpOne ({{date}}, '\\s+(\\d):', ' 0\\1:'),
-            '(\\d{2})\\.(\\d{2})\\.(\\d{4})', '\\3-\\2-\\1'), 'Europe/Moscow')
-{%- endmacro %}
