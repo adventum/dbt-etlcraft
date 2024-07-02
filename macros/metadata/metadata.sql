ba53ae278790c9735543627f900630eb7ef62edc
Malakhova-Natalya <nataviolin01121990@gmail.com>
2024-07-02 16:39:30 +0300

    features для вызова метадаты

--- macros/metadata/metadata.sql
+++ macros/metadata/metadata.sql
@@ -3,9 +3,9 @@
 {#- в этом макросе мы определяем, какая версия метадаты будет использоваться -#}
 
 {%- if metadata_name -%} {# либо используется та метадата, название которой передал пользователь #}
-    {{ etlcraft[metadata_name]() }}
+    {{ etlcraft[metadata_name](features = etlcraft.get_features()) }}
 {%- else -%} {# либо, если пользователь ничего не передал, используется последняя версия #}
-    {{ etlcraft.metadata_1() }}
+    {{ etlcraft.metadata_1(features = etlcraft.get_features()) }}
 {%- endif -%}
 
 {% endmacro %}
