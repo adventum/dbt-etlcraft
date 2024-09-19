8065395ac8bbb7b5ecfb2d5398c1047dc94ab0d2
Nikolay Suschenko <n.sushchenko@adventum.ru>
2024-09-19 14:37:38 +0300

    Удалил старые файлы

--- airflow_providers/apache_airflow_providers_etlcraft_dags/src/apache_airflow_providers_etlcraft_dags/configs/get_configs.py
+++ airflow_providers/apache_airflow_providers_etlcraft_dags/src/apache_airflow_providers_etlcraft_dags/configs/get_configs.py
@@ -1,221 +1,99 @@
-import json
 import yaml
-import re
-
+import json
+from collections import namedtuple
+from apache_airflow_providers_etlcraft_dags.exceptions import EtlcraftConfigError
+from apache_airflow_providers_etlcraft_defaults import get_etlcraft_defaults
 from airflow.models import Variable
-from airflow.utils.db import create_session
+import pathlib
+from enum import Enum
 
-from ..exceptions import EtlcraftConfigError
+Metaconfig = namedtuple("Metaconfig", "source format path")
+Source = Enum('Source', 'datacraft_variable templated_file file other_variable')
+Format = Enum('Format', 'json yaml')
 
-
-default_value_mapping = {
-    "connectors": {
-        "type": "templated_file",
-        "format": "yaml",
-    },
-    "presets": {
-        "type": "templated_file",
-        "format": "json",
-    },
-    "datasources": {
-        "type": "datacraft_variable",
-        "format": "json",
-    },
-    "metadata": {
-        "type": "templated_file",
-        "format": "yaml",
-    },
-    "events": {
-        "type": "templated_file",
-        "format": "yaml",
-    },
-    "attributions": {
-        "type": "datacraft_variable",
-        "format": "json",
-    },
-    "base": {
-        "type": "templated_file",
-        "format": "yaml",
-    }
-}
-
-
-# Функция, которая парсит метаконфиги и возвращает их значение
-# (Например из: source_for_config_etlcraft_from_datacraft)
-def etlcraft_variable(variable_id_without_prefix: str, namespace: str, default_value=None) -> str:
-    variable_id = f"{namespace}_{variable_id_without_prefix}"
-    return Variable.get(variable_id, default_var=default_value)
-
-
-def process_single(
-    config_name: str,
-    namespace: str,
-    source_key: str,
-    format_key: str,
-    path_key: str,
-    entire_datacraft_variable: bool = False,
-    collected_base_load: dict[str: dict] = None
-):
-
-    # Сначала определяем source, format и path для конфигурации
-    source = Variable.get(
-        source_key,
-        default_var=default_value_mapping.get(config_name).get("type")
-        if config_name in default_value_mapping.keys() else {}
-    )
-    file_format = Variable.get(
-        format_key,
-        default_var=default_value_mapping.get(config_name).get("format")
-        if config_name in default_value_mapping.keys() else {}
-    )
-    path = Variable.get(path_key, default_var="")
-
-    # В зависимости от источника, загружаем конфигурацию
-    if source in ["file", "templated_file"]:
-        if source == "templated_file":
-            path = f"templated_configs/{config_name}"
-
-        if source == "file" and not path:
-            path = f"configs/{config_name}"
-
-        # Если приходит конфиг base, то после того как он собрал свои подконфиги,
-        # он их выгружает по пути, указанным в метаконфиге path
-        if config_name == "base" and (source == "file" or source == "templated_file"):
-            path = Variable.get(path_key, default_var="")
-
-        if not path.endswith(('.json', '.yml', '.yaml')):
-            extension = ".json" if file_format == "json" else ".yml"
-            path += extension
-
-        try:
-            if config_name == "base":
-                # Выргрузка в указанный файл содержимого подконфигов
-                with open(path, 'w') as file:
-                    if file_format == "json":
-                        json.dump(collected_base_load, file)
-                    else:
-                        config = yaml.dump(collected_base_load, file)
-            else:
-                with open(path, 'r') as file:
-                    if file_format == "json":
-                        config = json.load(file)
-                    else:
-                        config = yaml.safe_load(file)
-        except FileNotFoundError:
-            raise EtlcraftConfigError(f"File '{path}' not found.")
-
-    elif source == "datacraft_variable":
-        json_datacraft_variable = Variable.get(f"from_datacraft{path}")
-        if isinstance(json_datacraft_variable, str):
-            json_datacraft_variable = json.loads(json_datacraft_variable)
-        if entire_datacraft_variable:
-            config = json_datacraft_variable
-        else:
-            config = json_datacraft_variable.get(config_name, {})
-            if config == {}:
-                raise EtlcraftConfigError(
-                    f"Config {config_name} not found in datacraft_variable, "
-                    f"check the contents of the variable "
-                    f"or check datacraft_variable name in Airflow"
-                )
-
-    elif source == "other_variable":
-
-        # Наполнение переменной, указанной в path_for_config_{namespace}_base, подконфигами base
-        if config_name == "base":
-            Variable.update(f"{path}", collected_base_load)
-
-        other_variable_path = f"{path}"
-        default_other_variable_path = f"{config_name}"
-        config = Variable.get(other_variable_path, default_var=default_other_variable_path)
-        if file_format == "json":
-            config = json.loads(config)
-        else:
-            config = yaml.safe_load(config)
-    else:
-        raise EtlcraftConfigError(f"Unknown source type: {source}")
-
-    return config
-
-
-def get_configs(
-    config_names: list[str] = None,
-    namespace: str = "etlcraft",
-    entire_datacraft_variable: bool = False
-) -> dict[str, dict]:
-
-    if config_names is None:
-        config_names = ["base",]
-
-    all_configs = {}
-
-    def get_single(config_name: str) -> dict:
-
-        source_key = f"source_for_config_{namespace}_{config_name}"
-        format_key = f"format_for_config_{namespace}_{config_name}"
-        path_key = f"path_for_config_{namespace}_{config_name}"
-
-        if config_name == "base":
-            #suffix = Variable.get(f"path_for_config_{namespace}_{config_name}")
-
-            with create_session() as session:
-
-                part_of_variables_need_to_find = f"{namespace}_base_"
-                # Полный список переменных
-                airflow_vars = {
-                    var.key: var.val for var in session.query(Variable)
-                }
-                # Отфильтрованный список переменных под конкретный base
-                filtered_vars = [
-                    key for key, value in airflow_vars.items()
-                    if part_of_variables_need_to_find in key
-                ]
-
-                pattern = rf"source_for_config_{namespace}_base_(.+)$"
-                suffixes = set()
-                for variable_name in filtered_vars:
-                    match = re.search(pattern, variable_name)
-                    if match:
-                        suffixes.add(match.group(1))
-
-                # Обработка для каждого подконфига base,
-                # по итогу получается 1 конфиг base, состоящий их подконфигов
-                all_base_config = {}
-                for suffix in suffixes:
-
-                    source_key = f"source_for_config_{namespace}_{config_name}_{suffix}"
-                    format_key = f"format_for_config_{namespace}_{config_name}_{suffix}"
-                    path_key = f"path_for_config_{namespace}_{config_name}_{suffix}"
-
-                    all_base_config[suffix] = process_single(
-                        suffix, namespace, source_key, format_key,
-                        path_key, entire_datacraft_variable
-                    )
-
-                # Добавление получившегося base в переменную {namespace}_base
-
-                source_base = f"source_for_config_{namespace}_base"
-                format_base = f"format_for_config_{namespace}_base"
-                path_base = f"path_for_config_{namespace}_base"
-
-                # Передача всех подконфигов base как единого словаря(all_base_config)
-                # для сохранения его в файле, или переменной(если source указан как other_variable),
-                # это зависит от переменной path_for_config_{namespace}_base в Админке Airflow
-                process_single(
-                    config_name, namespace, source_base, format_base,
-                    path_base, entire_datacraft_variable, all_base_config
-                )
-
-                return all_base_config
-        else:
-            config = process_single(
-                config_name, namespace, source_key, format_key,
-                path_key, entire_datacraft_variable
-            )
-
-            return config
-
+def get_configs(namespace: str, config_names=[]) -> dict:    
+    default_metaconfigs = get_single_config('metaconfigs', get_metaconfig(namespace, 'metaconfigs'))
+    base = get_single_config('base', get_metaconfig(namespace, 'base', default_metaconfigs))    
+    for base_var in base.keys():
+        overriden_val = process_single_config(namespace, f"base_{base_var}", default_metaconfigs, base)
+        if overriden_val:
+            base[base_var] = overriden_val    
+    if 'base' in config_names:
+        config_names.remove('base')
+    if 'metaconfigs' in config_names:
+        config_names.remove('metaconfigs')
+    all_configs = {'base': base}
     for config_name in config_names:
-        all_configs[config_name] = get_single(config_name)
+        all_configs[config_name] = process_single_config(namespace, config_name, default_metaconfigs, base)    
 
-    return all_configs
+def process_single_config(namespace: str, config_name: str, default_metaconfigs: Metaconfig, base_config: dict):
+    return get_single_config(config_name, get_metaconfig(namespace, config_name, default_metaconfigs), base_config)
+
+def get_default_path(source):
+    match source:
+        case Source.datacraft_variable | Source.templated_file:
+            return ''
+        case Source.file:
+            return f"configs/{source}"
+        case Source.other_variable:
+            return source
+    raise EtlcraftConfigError(f"unknown source: {source}")
+
+def get_metaconfig(namespace: str, config_name: str, defaults: None) -> Metaconfig:
+    if not defaults:
+        if config_name != 'metaconfigs':
+            raise EtlcraftConfigError("get_metaconfig is called without defaults. It is possible only for metaconfigs itself") 
+        else:
+            defaults = {"mataconfigs": {"source": "templated_file", "format": "json"}}
+            Metaconfig._fields
+    source, format, path = (Variable.get(f"{x}_for_{namespace}_{config_name}", default_var=defaults.get(config_name, {}).get(x)) for x in Metaconfig._fields)    
+    if source is None or format is None:
+        if config_name.startswith('base_'):
+            return None
+        raise EtlcraftConfigError(f'source or format not found for config {config_name} (namespace {namespace})')
+    source = Source[source]
+    format = Format[format]
+    if path is None:
+        path = get_default_path(source)
+    return Metaconfig(source, format, path)
+
+def parse_by_format(text, format):
+    match format:
+        case Format.yaml:
+            return yaml.safe_load(text)
+        case Format.json:
+            return json.loads(text)
+    raise EtlcraftConfigError(f"unknown format ({format})")
+    
+def get_single_config(config_name: str, metaconfig: Metaconfig, base_config=None):
+    if not base_config:
+        if config_name not in ('metaconfigs', 'base'):
+            raise EtlcraftConfigError("get_single_config is called without base_config. It is possible only for metaconfigs or base")
+        else:
+            base_config = {}
+    match metaconfig.source:
+        case Source.file:
+            filepath = pathlib.Path(metaconfig.path)
+            if not (filepath.exists() and filepath.is_file()):                
+                raise EtlcraftConfigError(f"file not found ({str(filepath)})")
+            content = filepath.read_text()
+            return parse_by_format(content, metaconfig.format)
+        case Source.templated_file:
+            filepath = pathlib.Path(metaconfig.path)
+            if not (filepath.exists() and filepath.is_file()):                
+                content = get_etlcraft_defaults(config_name, metaconfig.format, metaconfig.path, base_config)
+            else:
+                content = filepath.read_text()
+            return parse_by_format(content, metaconfig.format)
+        case Source.other_variable:
+            return parse_by_format(Variable.get(metaconfig.path), metaconfig.format)
+        case Source.datacraft_variable:
+            datacraft_variable = f"from_datacraft{metaconfig.path}"
+            var = parse_by_format(Variable.get(datacraft_variable), metaconfig.format)
+            ret = var.get(config_name)
+            if ret is None:
+                raise EtlcraftConfigError(f"key {config_name} not found in dataCraft variable {datacraft_variable}")
+            return ret
+
+            
+        
